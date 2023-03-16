06d59b09028d5ce1293fe8f3023aaf1a4a326e11
kay <kayannesouza14@gmail.com>
2023-03-16 19:36:50 -0300

    refactor: alter header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import Avatar from 'components/Avatar'
 import Card from 'components/Card'
+import {Header}  from 'components/header/header'
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="bg-red-100 min-h-screen items-center justify-center h-screen flex">
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex-row items-center space-x-4">
-        <div>
-          <Avatar />
+  
+    <div className="bg-red-100 min-h-screen h-screen">{/*header */}
+      <Header/>
+      
+
+      <main className='h-full flex justify-center items-center'>
+        <div className="p-6 max-w-sm bg-white rounded-xl space-x-4">
+          <div>
+            <Avatar />
+          </div>
+          <div className="mt-2">
+            <div className="text-xl font-medium text-black">Chat</div>
+            <p className="text-slate-500">You have a new message!</p>
+          </div>
+          <div className="mt-4">
+            <Card
+              title="Interest topic"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. LorIpsum.
+              "
+              /*parte do card localizado na tela  */
+            />
+          </div>
         </div>
-        <div className="mt-2">
-          <div className="text-xl font-medium text-black">Chat</div>
-          <p className="text-slate-500">You have a new message!</p>
-        </div>
-        <div className="mt-4">
-          <Card
-            title="Interest topic"
-            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. LorIpsum.
-            "
-          />
-        </div>
-      </div>
+      </main>
     </div>
+    
   )
 }
 
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,8 +4,8 @@ type CardProps = {
 }
 
 const Card = ({ description, title }: CardProps) => (
-  <div className="bg-black dark:bg-slate-500 rounded-lg px-6 py-4 ring-1 ring-slate-900/5 shadow-xl">
-    <h3 className="text-slate-700 dark:text-white text-base font-medium tracking-tight">
+  <div className="bg-white dark:bg-slate-200 rounded-lg px-6 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 className="text-slate-500 dark:text-white text-base font-medium tracking-tight">
       {title}
     </h3>
     <p className="text-slate-700 dark:text-slate-500 mt-2 text-sm">
