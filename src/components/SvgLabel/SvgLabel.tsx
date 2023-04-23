2e12f5b7fb9b137412161c1a20aad8c8cd00d876
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2023-04-23 15:35:01 -0300

    Creat SvgLabel component

--- src/components/SvgLabel/SvgLabel.tsx
+++ src/components/SvgLabel/SvgLabel.tsx
@@ -1,4 +1,16 @@
-// const SvgLabel = ({ svg, label }: SvgLabelProps) => (
-//     <>
-//     </>
-// )
+import { ReactComponent as MySvg } from '../../assets/svg/instagram-167-svgrepo-com.svg';
+
+type SvgLabelProps = {
+    svg: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    label: string;
+  };
+  
+  const SvgLabel = ({ svg: SvgComponent, label }: SvgLabelProps) => (
+    <div>
+      <SvgComponent />
+      <span>{label}</span>
+    </div>
+  );
+  
+  export default SvgLabel;
+  
